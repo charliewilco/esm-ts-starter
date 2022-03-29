ba6e1a80d224d01ab3bf1b953199fddb9996efa7
Charlie ⚡ <charliewilc0@protonmail.com>
2022-03-29 00:22:29 -0700

    simplifying rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -3,48 +3,31 @@ import dts from "rollup-plugin-dts";
 import esbuild from "rollup-plugin-esbuild";
 import { defineConfig } from "rollup";
 
-/**
- * @param {Omit<import('rollup').RollupOptions, 'input' | 'external' | 'plugins'>} config
- * @param {import('rollup').Plugin[]} plugins
- * @return {import('rollup').RollupOptions}
- **/
-const createOutputBundle = (config, plugins) => ({
-  ...config,
-  plugins,
-  input: "src/index.ts",
-  external: (id) => !/^[./]/.test(id),
-});
-
-/**
- * @type {import('rollup').RollupOptions[]}
- **/
-const config = [
-  createOutputBundle(
-    {
-      output: [
-        {
-          file: "dist/index.cjs.js",
-          format: "cjs",
-          sourcemap: true,
-        },
-        {
-          file: "dist/index.esm.js",
-          format: "es",
-          sourcemap: true,
-        },
-      ],
-    },
-    [esbuild()]
-  ),
-  createOutputBundle(
-    {
-      output: {
-        file: "dist/index.d.ts",
-        format: "es",
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    external: (id) => !/^[./]/.test(id),
+    output: [
+      {
+        file: "dist/index.cjs.js",
+        format: "cjs",
+        sourcemap: true,
       },
+      {
+        file: "dist/index.esm.js",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [esbuild()],
+  },
+  {
+    input: "src/index.ts",
+    external: (id) => !/^[./]/.test(id),
+    output: {
+      file: "dist/index.d.ts",
+      format: "es",
     },
-    [dts()]
-  ),
-];
-
-export default defineConfig(config);
+    plugins: [dts()],
+  },
+]);
