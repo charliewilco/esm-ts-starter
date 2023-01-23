cdcd9287599181cf9d2f56975aa6b18312d3b0a0
Charlie ⚡ <charliewilc0@protonmail.com>
2023-01-23 02:11:22 -0800

    updates to package.json

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,30 +4,30 @@ import esbuild from "rollup-plugin-esbuild";
 import { defineConfig } from "rollup";
 
 export default defineConfig([
-  {
-    input: "src/index.ts",
-    external: (id) => !/^[./]/.test(id),
-    output: [
-      {
-        file: "dist/index.cjs.js",
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: "dist/index.esm.js",
-        format: "es",
-        sourcemap: true,
-      },
-    ],
-    plugins: [esbuild()],
-  },
-  {
-    input: "src/index.ts",
-    external: (id) => !/^[./]/.test(id),
-    output: {
-      file: "dist/index.d.ts",
-      format: "es",
-    },
-    plugins: [dts()],
-  },
+	{
+		input: "src/index.ts",
+		external: (id) => !/^[./]/.test(id),
+		output: [
+			{
+				file: "dist/index.cjs",
+				format: "cjs",
+				sourcemap: true,
+			},
+			{
+				file: "dist/index.mjs",
+				format: "es",
+				sourcemap: true,
+			},
+		],
+		plugins: [esbuild()],
+	},
+	{
+		input: "src/index.ts",
+		external: (id) => !/^[./]/.test(id),
+		output: {
+			file: "dist/index.d.ts",
+			format: "es",
+		},
+		plugins: [dts()],
+	},
 ]);
